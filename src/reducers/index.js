4326f03f3c9b2869be84cb240ebd4265b0974927
YestoryFornever <guosj90@163.com>
2017-05-01 11:56:19 +0800

    add_todo reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,14 @@
-export default function fnReducers(state = [], action){
-	switch (action.type) {
-		default:
-			return state
+import { combineReducers } from 'redux';
+import { todos } from './todos.reducer';
+
+const fnReducers = combineReducers({
+	todos
+})
+
+export default fnReducers
+
+/*export default function fnReducers(state={},action){//不使用combineReducers的写法
+	return {
+		todos:todos(state.todos,action)
 	}
-}
+}*/
