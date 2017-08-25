eb7ec3a064f01472e34518b68a76c4e4dee8d537
yestoryfornever <guosj90@163.com>
2017-08-25 09:29:29 +0800

    launch.json

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 
 import fnReducers from './reducers';
 let store = createStore(fnReducers);
-
+// 将动作(action) 变换成 state 转换函数(reducer) ，然后放到一个统一的地方(store)来 setState
 import App from './containers/app';
 render(
 	<Provider store={store}>
